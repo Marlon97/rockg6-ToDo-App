669ec2f633af8c4f124253fa24ba19838a1d9537
Marlon97 <mb.pinello@gmail.com>
2021-07-01 00:55:08 -0500

    types, default data, some queries and mutations

--- gql.js
+++ gql.js
@@ -6,11 +6,28 @@ const typeDefs = gql`
   }
   
   type Subtask{
-      
+      id: String
+      category: String
+      title: String
+      description: String
+      status: String
+      start_date: String
+      end_date: String
+      finished_date: String
+      check_user_id: String
   }
   
-  type Task{
-      
+  type Task {
+      id: String
+      category: String
+      title: String
+      description: String
+      status: String
+      start_date: String
+      end_date: String
+      finished_date: String
+      check_user_id: String 
+      sub_tasks: [ Subtask ] 
   }
   
   type User {
@@ -23,17 +40,68 @@ const typeDefs = gql`
   type Query {
     books: [Book]
     users: [User]
+    user(id: String!): User
+    tasks: [Task]
   }
 
   type Mutation {
     addBook(title: String, author: String): Book
     addUser(id: String, nombre: String, correo: String, secret: String): User
+    addTask(category: String, title: String, description: String, status: String, start_date: String, end_date: String, finished_date: String, check_user_id: String): Task
+    addSubtask(category: String, title: String, description: String, status: String, start_date: String, end_date: String, finished_date: String, check_user_id: String): Task
+    updateUser(id: String, nombre: String, correo: String): User
   }
 `;
 
-const tasks = []
-
-const subtasks = []
+const tasks = [
+    {
+        id: "t1",
+        category: "cat1",
+        title: "title1",
+        description: "desc",
+        status: "stat",
+        start_date: "2021-06-28",
+        end_date: "2021-06-30",
+        finished_date: "2021-06-29",
+        check_user_id: "GH1000001",
+        sub_tasks: [
+            {
+                id: "t1_s1",
+                category: "cat1_1",
+                title: "title1_1",
+                description: "desc_1",
+                status: "stat_1",
+                start_date: "2021-06-29",
+                end_date: "2021-06-30",
+                finished_date: "2021-06-29",
+                check_user_id: "GH1000001_1"
+            },
+            {
+                id: "t1_s2",
+                category: "cat1_2",
+                title: "title1_2",
+                description: "desc_2",
+                status: "stat_2",
+                start_date: "2021-06-30",
+                end_date: "2021-06-30",
+                finished_date: "2021-06-30",
+                check_user_id: "GH1000001_2"
+            }
+        ]
+    },
+    {
+        id: "t2",
+        category: "cat2",
+        title: "title2",
+        description: "desc2",
+        status: "stat2",
+        start_date: "2022-06-28",
+        end_date: "2022-06-30",
+        finished_date: "2022-06-29",
+        check_user_id: "GH1000002",
+        sub_tasks: []
+    }
+];
 
 const users = [
     {
@@ -76,6 +144,8 @@ const resolvers = {
     Query: {
         books: () => books,
         users: () => users,
+        user: (_,args) => users.find(el => el.secret === args.secret),
+        tasks: () => tasks,
     },
     Mutation: {
         addBook: (_, args) => {
@@ -85,6 +155,18 @@ const resolvers = {
         },
         addUser: (_, args) => {
             console.log(args);
+            users.push({...args});
+            return 1;
+        },
+        addTask: (_, args) => {
+            console.log(args);
+            tasks.push({...args});
+        },
+        updateUser: (_, args) => {
+            const user = users.find(el => el.id === args.id);
+            user.nombre = args.nombre;
+            user.correo = args.correo;
+            return user;
         }
     }
 };
@@ -92,3 +174,4 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
+
