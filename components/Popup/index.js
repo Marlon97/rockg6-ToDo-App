529a20906728e16544f966c91f8a73f94e869b61
Jazmín Patrón Galindo <jazmin@MacBook-Pro-de-Jazmin.local>
2021-07-02 17:01:34 -0500

    Showing date and time in the mainview

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -27,9 +27,9 @@ const Popup = ({ onceSubmited, close }) => {
               placeholder="Your task description (optional)"
             ></textarea>
               <label>_______Start date:</label>
-              <input {...register("startDate")} type="datetime-local" id="start-meeting-time" name="start-meeting-time" onChange={() => {}} value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00" ></input>
+              <input {...register("startDate")} type="datetime-local" id="startDate" name="startDate"  min="2020-06-07T00:01" max="2022-06-14T00:00" ></input>
               <label>________End date:</label>
-              <input {...register("endDate")} type="datetime-local" id="end-meeting-time" name="end-meeting-time" onChange={() => {}} value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00" ></input>
+              <input {...register("endDate")} type="datetime-local" id="endDate" name="endDate"  min="2020-06-07T00:01" max="2022-06-14T00:00" ></input>
           </div>
           <input type="submit" className={styles["btn-submit"]} value="Add" />
         </form>
